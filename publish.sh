32da9fcdc4b840f08926737832011e5dbc862dec
Bryce Fisher-Fleig <brycefisher@users.noreply.github.com>
2015-10-30 23:35:32 -0700

    Initial travis commit
     * Use latest Jekyll inside a docker image to build site
     * Explicitly list gems that are depended on (already in docker image)
     * adds aws cli to travis
     * Refactored publish script for travis publishing to the stage S3 bucket
     * Show blog URL during travis build
     * Use new-fangled jekyll-assets gem to make sure sass gets compiled
     * Stop using compass (probably need to re-add the CSS reset later)

--- publish.sh
+++ publish.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
 
-aws s3 sync --profile=God --delete _site/ s3://bryce-fisher-fleig-org
+#######################################################
+# Requirements:
+# - valid S3 credentials with write permissions
+# - the aws cli (http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
+#
+# Usage:
+#
+#   ./publish.sh
+#
+# Publish to production:
+#
+#   ./publish.sh prod
+#######################################################
+
+PROD_BUCKET="bryce-fisher-fleig-org"
+PROD_URL="https://bryce.fisher-fleig.org/"
+STAGE_BUCKET="bryce-fisher-fleig-org-stage"
+STAGE_URL="http://bryce-fisher-fleig-org-stage.s3-website-us-west-1.amazonaws.com/"
+
+OUTPUT_URL=$STAGE_URL
+DEST_BUCKET=$STAGE_BUCKET
+if [ $1 = "prod" ]; then
+    DEST_BUCKET=$PROD_BUCKET
+    OUTPUT_URL=$PROD_URL
+fi
+
+echo "Publishing files from ./_site/ to S3 Bucket $DEST_BUCKET"
+aws s3 sync --delete _site/ "s3://$DEST_BUCKET"
+echo "Finished publishing to $DEST_BUCKET"
+echo "See $OUTPUT_URL to inspect the results"
