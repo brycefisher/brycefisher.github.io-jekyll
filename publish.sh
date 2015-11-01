9ce7a22561d02e12d8b46b80f1ff1425ef7694c9
Bryce Fisher-Fleig <bryce@fisher-fleig.org>
2015-11-01 13:15:04 -0800

    Show blog URL during travis build

--- publish.sh
+++ publish.sh
@@ -15,13 +15,18 @@
 #######################################################
 
 PROD_BUCKET=bryce-fisher-fleig-org
+PROD_URL="https://bryce.fisher-fleig.org/"
 STAGE_BUCKET=bryce-fisher-fleig-org-stage
+STAGE_URL="http://bryce-fisher-fleig-org-stage.s3-website-us-west-1.amazonaws.com/"
 
+OUTPUT_URL=STAGE_URL
 DEST_BUCKET=STAGE_BUCKET
 if $1=="prod" then
     DEST_BUCKET=PROD_BUCKET
+    OUTPUT_URL=PROD_URL
 fi
 
 echo "Publishing files from ./_site/ to S3 Bucket $DEST_BUCKET"
 aws s3 sync --delete _site/ "s3://$DEST_BUCKET"
 echo "Finished publishing to $DEST_BUCKET"
+echo "See $OUTPUT_URL to inspect the results"
