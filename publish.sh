7754329b704f66164a7f13a03852b17a8e976331
Bryce Fisher-Fleig <bryce@fisher-fleig.org>
2015-11-07 22:34:02 -0800

    Make uploaded files publically readable and cheaper

--- publish.sh
+++ publish.sh
@@ -21,6 +21,10 @@ DEST_URL="http://$DEST_BUCKET.s3-website-us-west-1.amazonaws.com/"
 echo "Publishing files from ./_site/ to S3 Bucket $DEST_BUCKET"
 aws s3 mb s3://$DEST_BUCKET --region us-west-1
 aws s3 website s3://$DEST_BUCKET --index-document "index.html"
-aws s3 sync --delete _site/ "s3://$DEST_BUCKET"
+aws s3 sync \
+    --delete \
+    --acl public-read \
+    --storage-class REDUCED_REDUNDANCY \
+    _site/ "s3://$DEST_BUCKET"
 echo "Finished publishing to $DEST_BUCKET. See results at:"
 echo $DEST_URL
